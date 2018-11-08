3f4f5eb9fa38725c10335cc7129a8e024c13dfa3
6Jonathan6 <jpr1993mm@hotmail.com>
2018-11-08 17:53:28 -0600

    working

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <template>
+  <div class="min-h-screen bg-grey-darker p-4">
+    <div style="width: 420px" class="mx-auto border-l border-r border-dashed border-grey-dark py-4 mb-4">
+      <width-dimensions>
+        <div slot-scope="{width}" class="profile-card" :class="{'profile-card-horizontal':width>400}">
+          <img class="profile-card-image" src="../static/img/toby.jpg" alt="">
+          <div class="profile-card-meta">
+            <div class="mb-4">
+              <p class="text-xl leading-tight">Toby Flenderson</p>
+              <p class="text-sm leading-tight text-grey-dark">Human Resources at Dunder Mifflin</p>
+            </div>
+            <div>
+              <button class="btn btn-sm btn-pill btn-indigo-outline">Message</button>
+            </div>
+          </div>
+        </div>
+      </width-dimensions>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import WidthDimensions from "@/components/WidthDimensions.vue";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  components: { WidthDimensions }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./assets/css/app.css"/>
---
+++ src/components/WidthDimensions.vue
@@ -0,0 +1,25 @@
+<script>
+import elementResizeDetectorMaker from "element-resize-detector";
+const erd = elementResizeDetectorMaker({ strategy: "scroll" });
+export default {
+  name: "WidthDimensions",
+  data() {
+    return {
+      width: null,
+      height: null
+    };
+  },
+  mounted() {
+    erd.listenTo(this.$el, el => {
+      this.width = el.offsetWidth;
+      this.height = el.offsetheight;
+    });
+  },
+  render() {
+    return this.$scopedSlots.default({
+      width: this.width,
+      height: this.height
+    });
+  }
+};
+</script>
